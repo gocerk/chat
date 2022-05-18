ac3fdec2c3f187a7708e5bb9a8c8ee9af0ccf5b0
kaanfkennedy <dev.kaanfkennedy@gmail.com>
2022-05-18 23:37:51 +0300

    Add service

---
+++ public/socketService.js
@@ -0,0 +1,19 @@
+const socket = io.connect('http://localhost:3000')
+
+const sendButton = document.getElementById('send-button');
+const chatOutput = document.getElementById('chat-output');
+const usernameInput = document.getElementById('username-input')
+const messageInput = document.getElementById('message-input');
+
+
+sendButton.addEventListener('click', () => {
+    console.log('clicked');
+    socket.emit('chatData', {
+        message: messageInput.value,
+        sender: usernameInput.value
+    });
+});
+
+socket.on('chatData', (data) => {
+    chatOutput.innerHTML += `<p><strong>${data.sender}: </strong>${data.message}</p>`
+});
